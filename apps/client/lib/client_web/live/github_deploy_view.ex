778f26e0eda05e084905cc4429b5a8e164f579c4
John Nicholas <john.nicholas@dinoroar.io>
2019-06-25 22:17:42 +0100

    pulled the live eex out into template

--- apps/client/lib/client_web/live/github_deploy_view.ex
+++ apps/client/lib/client_web/live/github_deploy_view.ex
@@ -2,16 +2,7 @@ defmodule ClientWeb.GithubDeployView do
   use Phoenix.LiveView
 
   def render(assigns) do
-    ~L"""
-    <div class="">
-      <div>
-      <div>
-        <button phx-click="github_deploy">Deploy to GitHub</button>
-      </div>
-      Status: <%= @deploy_step %>
-      </div>
-    </div>
-    """
+    ClientWeb.PageView.render("github_deploy.html", assigns)
   end
 
   def mount(_session, socket) do
